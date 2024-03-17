a9b3ea024faa42b2d31c1b744208d543c2394c8b
AlanGF7 <af189107@gmail.com>
2024-03-16 23:46:21 -0600

    Home
    Header creado
    Body de "Home"
    Footer creado

--- FrontEnd/BookNookery-Angular/src/app/components/home/home.component.ts
+++ FrontEnd/BookNookery-Angular/src/app/components/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component'
+import { HeaderComponent } from '../header/header.component'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit  {
 
+    constructor(public Footer: FooterComponent, public Header: HeaderComponent) {}
+
+
+    ngOnInit(): void {
+        
+    }
 }
